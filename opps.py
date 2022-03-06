f6bfee410eacf22fd6e0e6b9e2964b2ba6ee9194
ferhatSF <32377618+ferhatSF@users.noreply.github.com>
2022-03-06 13:23:25 -0800

    Update opps.py

--- opps.py
+++ opps.py
@@ -99,7 +99,7 @@ shows[DATE_COLUMN] = pd.to_datetime(shows[DATE_COLUMN]).dt.year
 shows=pd.pivot_table(shows, values='Amount', index=DATE_COLUMN,
                     columns='Lead Source', aggfunc=np.sum)
 shows=shows.fillna(0)
-shows.set_index(DATE_COLUMN)
+shows=shows.set_index(DATE_COLUMN)
 st.table(shows)
 st.bar_chart(shows)
 
