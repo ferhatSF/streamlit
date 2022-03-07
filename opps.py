1abfb0450b35297ea8e5be2b07948bfaaf366365
ferhatSF <32377618+ferhatSF@users.noreply.github.com>
2022-03-06 18:43:58 -0800

    Update opps.py

--- opps.py
+++ opps.py
@@ -4,22 +4,6 @@ import pandas as pd
 
 from functionforDownloadButtons import download_button
 
-###################################
-
-
-def _max_width_():
-    max_width_str = f"max-width: 1800px;"
-    st.markdown(
-        f"""
-    <style>
-    .reportview-container .main .block-container{{
-        {max_width_str}
-    }}
-    </style>    
-    """,
-        unsafe_allow_html=True,
-    )
-
 st.set_page_config(page_icon="✂️", page_title="PULSE REPORT: OPPS")
 
 
