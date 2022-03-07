2c5ba75dd23411d855397ec9bb848716aaac0b8e
ferhatSF <32377618+ferhatSF@users.noreply.github.com>
2022-03-06 18:43:21 -0800

    Update opps.py

--- opps.py
+++ opps.py
@@ -2,13 +2,6 @@ import streamlit as st
 import numpy as np
 import pandas as pd
 
-###################################
-from st_aggrid import AgGrid
-from st_aggrid.grid_options_builder import GridOptionsBuilder
-from st_aggrid.shared import JsCode
-
-###################################
-
 from functionforDownloadButtons import download_button
 
 ###################################
