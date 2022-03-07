e2ac5a4058b3098b4351d57e9d4535093fe22967
ferhatSF <32377618+ferhatSF@users.noreply.github.com>
2022-03-07 15:34:03 -0800

    Update timeline_analytics_csv.py

--- timeline_analytics_csv.py
+++ timeline_analytics_csv.py
@@ -22,6 +22,7 @@ st.set_page_config(page_icon="💲", page_title="PULSE REPORT: OPPS")
 st.title("CVS Timeline Data Analytics")
 
 sample_file="https://raw.githubusercontent.com/ferhatSF/sample-data/5e641880a6767affc2798aa9be7cd99c5739d247/sample_timeline.csv"
+sample = pd.read_csv(sample_file)
 
 c29, c30, c31 = st.columns([1, 6, 1])
 
@@ -33,7 +34,7 @@ with c30:
         help="To activate 'wide mode', go to the hamburger menu > Settings > turn on 'wide mode'",
     )
     st.info("👆 Upload a .csv file here. Sample to try: Sample-Timeline-Analytics.csv " + sample_file )
-    st.download_button('Download CSV', sample_file, 'text/csv')   
+    st.download_button('Download CSV', sample, 'text/csv')   
     if uploaded_file is not None:
         file_container = st.expander("Check your Timeline data .csv")
         shows = pd.read_csv(uploaded_file)
