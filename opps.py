5c28850b7509a2d86850641e7f51294ae6116eb3
ferhatSF <32377618+ferhatSF@users.noreply.github.com>
2022-03-06 17:22:41 -0800

    Update opps.py

--- opps.py
+++ opps.py
@@ -95,14 +95,18 @@ DATE_COLUMN='Close Date'
 shows=shows[shows['Stage'].str.contains("Won")]
 
 shows = shows[[DATE_COLUMN,'Lead Source','Amount']]
+
+st.write(df.columns)
+
 shows[DATE_COLUMN] = pd.to_datetime(shows[DATE_COLUMN]).dt.year
+
 df=pd.pivot_table(shows, values='Amount', index=DATE_COLUMN,
                     columns='Lead Source', aggfunc=np.sum)
 
-st.write(df)
+st.write(df.columns)
 
 df=df.fillna(0)
-st.table(df)
+st.table(df.columns)
 #st.bar_chart(df)
 
 st.write(df.columns)
